67dd512053d6e75940a0485f4024f80aea7efee5
theharlotfelon <george.guerrero@gmail.com>
2023-09-14 14:56:36 -0500

    refactor code to move about-us to correct folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home";
-import {AboutUsComponent} from "./about-us/about-us.component";
+import {AboutUsComponent} from "./pages/about-us/about-us.component";
 import {CallbackComponent} from "./_helpers/callback.component";
 import {AuthGuard} from "./auth/auth.guard";
 
